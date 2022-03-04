dcc47e82f329f779f414312c8562b19d9e8955ba
mrepasky3 <mwrepasky@gmail.com>
2022-03-04 12:59:13 -0500

    fixes, add report files in results

--- driver.py
+++ driver.py
@@ -28,9 +28,9 @@ parser.set_defaults(feature=False)
 
 if __name__ == '__main__':
 	stop_words = stopwords.words('english')
-    stop_words.extend(['im','ive','dont','get','youre','would','thats',
-                       'really','one','also','something','even','thing','things','must',
-                       'cant','much','could','way','lot','got','get','go','like','th'])
+	stop_words.extend(['im','ive','dont','get','youre','would','thats',
+		'really','one','also','something','even','thing','things','must',
+		'cant','much','could','way','lot','got','get','go','like','th'])
 
 	args = parser.parse_args()
 	if not os.path.exists("results"):
@@ -38,28 +38,35 @@ if __name__ == '__main__':
 
 	comments_list = []
 	for data_file in os.listdir('weekly_data'):
-	    loaded_comments = pd.read_csv('weekly_data/' + data_file)
-	    comments_list += list(loaded_comments['body'])
+		loaded_comments = pd.read_csv('weekly_data/' + data_file)
+		comments_list += list(loaded_comments['body'])
 
 	comments_words, bigram_model, trigram_model = tokenize_lda(data_cleaning(comments_list))
 	comments_bow, dictionary = create_dictionary(comments_words)
 
 	lda = gensim.models.LdaMulticore(corpus=comments_bow, id2word=dictionary, num_topics=args.n_topics,
-                               chunksize=500, passes=10, alpha='asymmetric', eta='auto')
+							   chunksize=500, passes=10, alpha='asymmetric', eta='auto')
 
 	# Compute Coherence Score
 	coherence_model = CoherenceModel(model=lda, texts=comments_words, dictionary=dictionary, coherence='c_v')
 	print("LDA Coherence: {:.3f}".format(coherence_model.get_coherence()))
 	print("LDA Per-Topic Coherence: "+str(coherence_model.get_coherence_per_topic()))
 
+	report_file = open("results/lda_coherence.txt", "w")
+	report_file.write("LDA Coherence: {:.3f}\n".format(coherence_model.get_coherence()))
+	per_topic_coherence = coherence_model.get_coherence_per_topic()
+	for i in range(len(per_topic_coherence)):
+		report_file.write("Topic {} Coherence: {:.3f}\n".format(i, per_topic_coherence[i]))
+	report_file.close()
+
 	topics = lda.show_topics(formatted=False, num_words=20)
 	fig, axes = plt.subplots(3, 3, figsize=(20,8))
 	cloud = WordCloud(stopwords=stop_words,background_color='white')
 	for i, ax in enumerate(axes.flatten()):
-	    cloud.generate_from_frequencies(dict(topics[i][1]), max_font_size=300)
-	    ax.imshow(cloud)
-	    ax.set_title('Topic ' + str(i), fontsize=26)
-	    ax.axis('off')
+		cloud.generate_from_frequencies(dict(topics[i][1]), max_font_size=300)
+		ax.imshow(cloud)
+		ax.set_title('Topic ' + str(i), fontsize=26)
+		ax.axis('off')
 	plt.tight_layout()
 	plt.savefig("results/LDA_Word_Cloud.png")
 	plt.close()
@@ -72,87 +79,92 @@ if __name__ == '__main__':
 		
 		comments_list_weekly = dict()
 		for data_file in os.listdir('weekly_data'):
-		    start = data_file.split('-')[0][-10:]
-		    start_time = int(dt.datetime.strptime(start, '%d_%m_%Y').timestamp())
-		    
-		    loaded_comments = list(pd.read_csv('weekly_data/' + data_file)['body'])
-		    tokenized_loaded_comments = prep_for_lda(loaded_comments)
-		    bow_loaded_comments = [dictionary.doc2bow(comment) for comment in tokenized_loaded_comments]
-		    comments_list_weekly[start_time] = bow_loaded_comments
+			start = data_file.split('-')[0][-10:]
+			start_time = int(dt.datetime.strptime(start, '%d_%m_%Y').timestamp())
+			
+			loaded_comments = list(pd.read_csv('weekly_data/' + data_file)['body'])
+			tokenized_loaded_comments = prep_for_lda(loaded_comments)
+			bow_loaded_comments = [dictionary.doc2bow(comment) for comment in tokenized_loaded_comments]
+			comments_list_weekly[start_time] = bow_loaded_comments
 		
 		topic_multiplicity_weekly = dict()
 		for key in comments_list_weekly.keys():
-		    topic_multiplicity_weekly[key] = np.zeros(9)
-		    this_week_comments = comments_list_weekly[key]
-		    for comment in this_week_comments:
-		        topic_dist = lda.get_document_topics(comment,per_word_topics=False)
-		        top_topic_idx = np.argmax(np.array(topic_dist)[:,1])
-		        top_topic = topic_dist[top_topic_idx][0]
-		        topic_multiplicity_weekly[key][top_topic] += 1
+			topic_multiplicity_weekly[key] = np.zeros(9)
+			this_week_comments = comments_list_weekly[key]
+			for comment in this_week_comments:
+				topic_dist = lda.get_document_topics(comment,per_word_topics=False)
+				top_topic_idx = np.argmax(np.array(topic_dist)[:,1])
+				top_topic = topic_dist[top_topic_idx][0]
+				topic_multiplicity_weekly[key][top_topic] += 1
 
 		topic_trends = []
 		for key in topic_multiplicity_weekly.keys():
-		    topic_trends.append([key]+list(topic_multiplicity_weekly[key]))
+			topic_trends.append([key]+list(topic_multiplicity_weekly[key]))
 		topic_trends = np.array(topic_trends)
 		sorted_idx = np.argsort(topic_trends[:,0])
 		sorted_topic_trends = topic_trends[sorted_idx]
 		topic_trend_dates = [dt.datetime.fromtimestamp(stamp) for stamp in sorted_topic_trends[:,0]]
 
-        color_list = ['tab:blue', 'tab:orange', 'tab:green', 'tab:purple',
-                      'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive', 'tab:cyan']
+		color_list = ['tab:blue', 'tab:orange', 'tab:green', 'tab:purple',
+					  'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive', 'tab:cyan']
 
 		fig, axs = plt.subplots(3, 3, figsize=(24,15))
 		k = 0
 		for i in range(3):
-		    for j in range(3):
-		        axs[i,j].xaxis.set_major_formatter(matplotlib.dates.DateFormatter('%b %Y'))
-		        axs[i,j].scatter(topic_trend_dates, sorted_topic_trends[:,k+1], marker='o', color=color_list[k])
-		        axs[i,j].plot(topic_trend_dates, sorted_topic_trends[:,k+1], ls='--', color=color_list[k])
-		        axs[i,j].tick_params(labelsize=18)
-		        axs[i,j].tick_params(axis='x', rotation=25)
-		        axs[i,j].set_title("Topic {}".format(k), fontsize=20)
-		        k+=1
+			for j in range(3):
+				axs[i,j].xaxis.set_major_formatter(matplotlib.dates.DateFormatter('%b %Y'))
+				axs[i,j].scatter(topic_trend_dates, sorted_topic_trends[:,k+1], marker='o', color=color_list[k])
+				axs[i,j].plot(topic_trend_dates, sorted_topic_trends[:,k+1], ls='--', color=color_list[k])
+				axs[i,j].tick_params(labelsize=18)
+				axs[i,j].tick_params(axis='x', rotation=25)
+				axs[i,j].set_title("Topic {}".format(k), fontsize=20)
+				k+=1
 		plt.tight_layout(pad=3.0) 
 		plt.savefig("results/weekly_trends.png")
 		plt.close()
 		plt.clf()
 
+		weekly_period_file = open("results/weekly_periods.txt", "w")
 		fig, axs = plt.subplots(3, 3, figsize=(24,15))
 		k = 0
 		for i in range(3):
-		    for j in range(3):
-		        freqs, power_spectral_density = signal.periodogram(sorted_topic_trends[:,k+1])
-		        power_spectrum_idx = np.argsort(power_spectral_density)
-		        print("Top {} Top Five Periods (weeks): ".format(k) + str(1/freqs[power_spectrum_idx][::-1][:5]))
-		        axs[i,j].semilogy(freqs, np.sqrt(power_spectral_density), color=color_list[k])
-		        axs[i,j].set_ylim([1e0, 1e3])
-		        axs[i,j].tick_params(labelsize=18)
-		        axs[i,j].set_title("Topic {}".format(k), fontsize=20)
-		        k+=1
+			for j in range(3):
+				freqs, power_spectral_density = signal.periodogram(sorted_topic_trends[:,k+1])
+				power_spectrum_idx = np.argsort(power_spectral_density)
+				weekly_period_file.write("Topic {} Top Five Periods (weeks): ".format(k) + str(1/freqs[power_spectrum_idx][::-1][:5]) + "\n")
+				axs[i,j].semilogy(freqs, np.sqrt(power_spectral_density), color=color_list[k])
+				axs[i,j].set_ylim([1e0, 1e3])
+				axs[i,j].tick_params(labelsize=18)
+				axs[i,j].set_title("Topic {}".format(k), fontsize=20)
+				k+=1
 		plt.tight_layout(pad=3.0) 
 		plt.savefig("results/weekly_periodogram.png")
 		plt.close()
 		plt.clf()
+		weekly_period_file.close()
 
 
+		weekly_slope_file = open("results/weekly_slopes.txt", "w")
 		fig, axs = plt.subplots(3, 3, figsize=(24,15))
 		time_weeks = ((sorted_topic_trends[:,0] - sorted_topic_trends[0,0])/(7*24*3600)).reshape(-1,1)
 		k = 0
 		for i in range(3):
-		    for j in range(3):
-		        axs[i,j].xaxis.set_major_formatter(matplotlib.dates.DateFormatter('%b %Y'))
-		        reg = TheilSenRegressor(n_jobs=-1).fit(time_weeks, sorted_topic_trends[:,k+1])
-		        axs[i,j].plot(topic_trend_dates, reg.intercept_ + reg.coef_[0]*time_weeks.flatten(), c='r',label="Slope: {:.2e}".format(reg.coef_[0]))
-		        axs[i,j].plot(topic_trend_dates, sorted_topic_trends[:,k+1], ls='-', color=color_list[k])
-		        axs[i,j].tick_params(labelsize=18)
-		        axs[i,j].tick_params(axis='x', rotation=25)
-		        axs[i,j].set_title("Topic {}".format(k), fontsize=20)
-		        axs[i,j].legend(fontsize=16)
-		        k+=1
+			for j in range(3):
+				axs[i,j].xaxis.set_major_formatter(matplotlib.dates.DateFormatter('%b %Y'))
+				reg = TheilSenRegressor(n_jobs=-1).fit(time_weeks, sorted_topic_trends[:,k+1])
+				weekly_slope_file.write("Topic {} Slope: {:.4e}\n".format(k, reg.coef_[0]))
+				axs[i,j].plot(topic_trend_dates, reg.intercept_ + reg.coef_[0]*time_weeks.flatten(), c='r',label="Slope: {:.2e}".format(reg.coef_[0]))
+				axs[i,j].plot(topic_trend_dates, sorted_topic_trends[:,k+1], ls='-', color=color_list[k])
+				axs[i,j].tick_params(labelsize=18)
+				axs[i,j].tick_params(axis='x', rotation=25)
+				axs[i,j].set_title("Topic {}".format(k), fontsize=20)
+				axs[i,j].legend(fontsize=16)
+				k+=1
 		plt.tight_layout(pad=3.0) 
 		plt.savefig("results/weekly_theilsen.png")
 		plt.close()
 		plt.clf()
+		weekly_slope_file.close()
 
 
 
@@ -161,88 +173,93 @@ if __name__ == '__main__':
 
 		full_dataframe = pd.DataFrame()
 		for data_file in os.listdir('weekly_data'):
-		    loaded_comments = pd.read_csv('weekly_data/' + data_file)
-		    full_dataframe = pd.concat([full_dataframe,loaded_comments], axis=0)
+			loaded_comments = pd.read_csv('weekly_data/' + data_file)
+			full_dataframe = pd.concat([full_dataframe,loaded_comments], axis=0)
 
 		num_days = (topic_trend_dates[-1] - topic_trend_dates[0] + dt.timedelta(days=7)).days
 		start_datetime = topic_trend_dates[0]
 		comments_list_daily = dict()
 		for i in range(num_days):
-		    this_start = (start_datetime + dt.timedelta(days=i)).timestamp()
-		    this_end = (start_datetime + dt.timedelta(days=1+i)).timestamp()
-		    loaded_comments = list(full_dataframe[full_dataframe['created_utc'].between(this_start,this_end)]['body'])
-		    tokenized_loaded_comments = prep_for_lda(loaded_comments)
-		    bow_loaded_comments = [dictionary.doc2bow(comment) for comment in tokenized_loaded_comments]
-		    comments_list_daily[this_start] = bow_loaded_comments
+			this_start = (start_datetime + dt.timedelta(days=i)).timestamp()
+			this_end = (start_datetime + dt.timedelta(days=1+i)).timestamp()
+			loaded_comments = list(full_dataframe[full_dataframe['created_utc'].between(this_start,this_end)]['body'])
+			tokenized_loaded_comments = prep_for_lda(loaded_comments)
+			bow_loaded_comments = [dictionary.doc2bow(comment) for comment in tokenized_loaded_comments]
+			comments_list_daily[this_start] = bow_loaded_comments
 
 		topic_multiplicity_daily = dict()
 		for key in comments_list_daily.keys():
-		    topic_multiplicity_daily[key] = np.zeros(9)
-		    this_day_comments = comments_list_daily[key]
-		    for comment in this_day_comments:
-		        topic_dist = lda.get_document_topics(comment,per_word_topics=False)
-		        top_topic_idx = np.argmax(np.array(topic_dist)[:,1])
-		        top_topic = topic_dist[top_topic_idx][0]
-		        topic_multiplicity_daily[key][top_topic] += 1
+			topic_multiplicity_daily[key] = np.zeros(9)
+			this_day_comments = comments_list_daily[key]
+			for comment in this_day_comments:
+				topic_dist = lda.get_document_topics(comment,per_word_topics=False)
+				top_topic_idx = np.argmax(np.array(topic_dist)[:,1])
+				top_topic = topic_dist[top_topic_idx][0]
+				topic_multiplicity_daily[key][top_topic] += 1
 
 		daily_topic_trends = []
 		for key in topic_multiplicity_daily.keys():
-		    daily_topic_trends.append([key]+list(topic_multiplicity_daily[key]))
+			daily_topic_trends.append([key]+list(topic_multiplicity_daily[key]))
 		daily_topic_trends = np.array(daily_topic_trends)
 		daily_sorted_idx = np.argsort(daily_topic_trends[:,0])
 		sorted_daily_topic_trends = daily_topic_trends[daily_sorted_idx]
 		daily_topic_trend_dates = [dt.datetime.fromtimestamp(stamp) for stamp in sorted_daily_topic_trends[:,0]]
 
-        color_list = ['tab:blue', 'tab:orange', 'tab:green', 'tab:purple',
-                      'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive', 'tab:cyan']
+		color_list = ['tab:blue', 'tab:orange', 'tab:green', 'tab:purple',
+					  'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive', 'tab:cyan']
 
 		fig, axs = plt.subplots(3, 3, figsize=(24,15))
 		k = 0
 		for i in range(3):
-		    for j in range(3):
-		        axs[i,j].xaxis.set_major_formatter(matplotlib.dates.DateFormatter('%b %Y'))
-		        axs[i,j].plot(daily_topic_trend_dates, sorted_daily_topic_trends[:,k+1], ls='-', color=color_list[k])
-		        axs[i,j].tick_params(labelsize=18)
-		        axs[i,j].tick_params(axis='x', rotation=25)
-		        axs[i,j].set_title("Topic {}".format(k), fontsize=20)
-		        k+=1
+			for j in range(3):
+				axs[i,j].xaxis.set_major_formatter(matplotlib.dates.DateFormatter('%b %Y'))
+				axs[i,j].plot(daily_topic_trend_dates, sorted_daily_topic_trends[:,k+1], ls='-', color=color_list[k])
+				axs[i,j].tick_params(labelsize=18)
+				axs[i,j].tick_params(axis='x', rotation=25)
+				axs[i,j].set_title("Topic {}".format(k), fontsize=20)
+				k+=1
 		plt.tight_layout(pad=3.0) 
 		plt.savefig("results/daily_trends.png")
 		plt.close()
 		plt.clf()
 
+		daily_period_file = open("results/daily_periods.txt", "w")
 		fig, axs = plt.subplots(3, 3, figsize=(24,15))
 		k = 0
 		for i in range(3):
-		    for j in range(3):
-		        freqs, power_spectral_density = signal.periodogram(sorted_daily_topic_trends[:,k+1])
-		        power_spectrum_idx = np.argsort(power_spectral_density)
-		        print("Top {} Top Five Periods (days): ".format(k) + str(1/freqs[power_spectrum_idx][::-1][:5]))
-		        axs[i,j].semilogy(freqs, np.sqrt(power_spectral_density), color=color_list[k])
-		        axs[i,j].set_ylim([1e-1, 1e3])
-		        axs[i,j].tick_params(labelsize=18)
-		        axs[i,j].set_title("Topic {}".format(k), fontsize=20)
-		        k+=1
+			for j in range(3):
+				freqs, power_spectral_density = signal.periodogram(sorted_daily_topic_trends[:,k+1])
+				power_spectrum_idx = np.argsort(power_spectral_density)
+				daily_period_file.write("Topic {} Top Five Periods (days): ".format(k) + str(1/freqs[power_spectrum_idx][::-1][:5]) + "\n")
+				axs[i,j].semilogy(freqs, np.sqrt(power_spectral_density), color=color_list[k])
+				axs[i,j].set_ylim([1e-1, 1e3])
+				axs[i,j].tick_params(labelsize=18)
+				axs[i,j].set_title("Topic {}".format(k), fontsize=20)
+				k+=1
 		plt.tight_layout(pad=3.0) 
 		plt.savefig("results/daily_periodogram.png")
 		plt.close()
 		plt.clf()
+		daily_period_file.close()
 
+		daily_slope_file = open("results/daily_slopes.txt", "w")
 		fig, axs = plt.subplots(3, 3, figsize=(24,15))
 		time_days = ((sorted_daily_topic_trends[:,0] - sorted_daily_topic_trends[0,0])/(24*3600)).reshape(-1,1)
 		k = 0
 		for i in range(3):
-		    for j in range(3):
-		        axs[i,j].xaxis.set_major_formatter(matplotlib.dates.DateFormatter('%b %Y'))
-		        reg = TheilSenRegressor(n_jobs=-1).fit(time_days, sorted_daily_topic_trends[:,k+1])
-		        axs[i,j].plot(daily_topic_trend_dates, reg.intercept_ + reg.coef_[0]*time_days.flatten(), c='r',label="Slope: {:.2e}".format(reg.coef_[0]))
-		        axs[i,j].plot(daily_topic_trend_dates, sorted_daily_topic_trends[:,k+1], ls='-', color=color_list[k])
-		        axs[i,j].tick_params(labelsize=18)
-		        axs[i,j].tick_params(axis='x', rotation=25)
-		        axs[i,j].set_title("Topic {}".format(k), fontsize=20)
-		        axs[i,j].legend(fontsize=16)
-		        k+=1
+			for j in range(3):
+				axs[i,j].xaxis.set_major_formatter(matplotlib.dates.DateFormatter('%b %Y'))
+				reg = TheilSenRegressor(n_jobs=-1).fit(time_days, sorted_daily_topic_trends[:,k+1])
+				daily_slope_file.write("Topic {} Slope: {:.4e}\n".format(k, reg.coef_[0]))
+				axs[i,j].plot(daily_topic_trend_dates, reg.intercept_ + reg.coef_[0]*time_days.flatten(), c='r',label="Slope: {:.2e}".format(reg.coef_[0]))
+				axs[i,j].plot(daily_topic_trend_dates, sorted_daily_topic_trends[:,k+1], ls='-', color=color_list[k])
+				axs[i,j].tick_params(labelsize=18)
+				axs[i,j].tick_params(axis='x', rotation=25)
+				axs[i,j].set_title("Topic {}".format(k), fontsize=20)
+				axs[i,j].legend(fontsize=16)
+				k+=1
 		plt.tight_layout(pad=3.0)
 		plt.savefig("results/daily_theilsen.png")
 		plt.close()
 		plt.clf()
+		daily_slope_file.close()
--- utils.py
+++ utils.py
@@ -7,138 +7,136 @@ import numpy as np
 import gensim
 from gensim.utils import simple_preprocess
 import gensim.corpora as corpora
-import nltk
-nltk.download('stopwords')
 from nltk.corpus import stopwords
 
 
 
 
 def data_cleaning(comments_list):
-    '''
-    Remove links, control sequences, emojis, and likely bots
+	'''
+	Remove links, control sequences, emojis, and likely bots
 
-    Parameters
-    ----------
-    comments_list : list of strings of raw comments
+	Parameters
+	----------
+	comments_list : list of strings of raw comments
 
-    Returns
-    -------
-    cleaned_comments : list of strings of cleaned comments
-    '''
+	Returns
+	-------
+	cleaned_comments : list of strings of cleaned comments
+	'''
 
-    cleaned_comments = []
-    for i in range(len(comments_list)):
-        # remove brackets
-        semi_clean = np.array(re.sub('\]',' ',re.sub('\[','',comments_list[i])).split())
-        # remove links
-        mask = [('http' not in word) and ('Http' not in word) and ('www.' not in word) and ('Www.' not in word) and ('.com' not in word) for word in semi_clean]
-        semi_clean = " ".join(list(semi_clean[mask]))
-        # remove control sequences
-        semi_clean = re.sub('&gt;','',semi_clean)
-        semi_clean = re.sub('&amp;','',semi_clean)
-        semi_clean = re.sub('\n','',semi_clean)
-        semi_clean = re.sub("\'","",semi_clean)
-        semi_clean = re.sub("\*","",semi_clean)
-        semi_clean = semi_clean.replace("\\","")
-        semi_clean = re.sub("’","",semi_clean)
-        # remove emojis
-        # from https://gist.github.com/slowkow/7a7f61f495e3dbb7e3d767f97bd7304b
-        emoji_pattern = re.compile("["
-                               u"\U0001F600-\U0001F64F"  # emoticons
-                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
-                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-                               u"\U00002702-\U000027B0"
-                               u"\U000024C2-\U0001F251"
-                               u"\U0001F973" # part hat emoji
-                               u"\U0001F97A" # sad eyes emoji
-                               u"\U0001F970" # smiling face with 3 hearts
-                               "]+", flags=re.UNICODE)
-        semi_clean = re.sub(emoji_pattern,r'', semi_clean)
-        # remove punctuation and symbols
-        clean = re.sub('[./&%$#()!?;:]', '', semi_clean).lower()
-        # remove empty comments and comments posted by bots
-        if (len(clean) > 0) and ('ɴᴏᴡ ᴘʟᴀʏɪɴɢ' not in clean) and ('bleep' not in clean) and ('beep' not in clean):
-            if ('[view link]' not in clean) and ('i am a bot' not in clean) and ('dmca removal request for' not in clean):
-                cleaned_comments.append(clean)
+	cleaned_comments = []
+	for i in range(len(comments_list)):
+		# remove brackets
+		semi_clean = np.array(re.sub('\]',' ',re.sub('\[','',comments_list[i])).split())
+		# remove links
+		mask = [('http' not in word) and ('Http' not in word) and ('www.' not in word) and ('Www.' not in word) and ('.com' not in word) for word in semi_clean]
+		semi_clean = " ".join(list(semi_clean[mask]))
+		# remove control sequences
+		semi_clean = re.sub('&gt;','',semi_clean)
+		semi_clean = re.sub('&amp;','',semi_clean)
+		semi_clean = re.sub('\n','',semi_clean)
+		semi_clean = re.sub("\'","",semi_clean)
+		semi_clean = re.sub("\*","",semi_clean)
+		semi_clean = semi_clean.replace("\\","")
+		semi_clean = re.sub("’","",semi_clean)
+		# remove emojis
+		# from https://gist.github.com/slowkow/7a7f61f495e3dbb7e3d767f97bd7304b
+		emoji_pattern = re.compile("["
+							   u"\U0001F600-\U0001F64F"  # emoticons
+							   u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+							   u"\U0001F680-\U0001F6FF"  # transport & map symbols
+							   u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+							   u"\U00002702-\U000027B0"
+							   u"\U000024C2-\U0001F251"
+							   u"\U0001F973" # part hat emoji
+							   u"\U0001F97A" # sad eyes emoji
+							   u"\U0001F970" # smiling face with 3 hearts
+							   "]+", flags=re.UNICODE)
+		semi_clean = re.sub(emoji_pattern,r'', semi_clean)
+		# remove punctuation and symbols
+		clean = re.sub('[./&%$#()!?;:]', '', semi_clean).lower()
+		# remove empty comments and comments posted by bots
+		if (len(clean) > 0) and ('ɴᴏᴡ ᴘʟᴀʏɪɴɢ' not in clean) and ('bleep' not in clean) and ('beep' not in clean):
+			if ('[view link]' not in clean) and ('i am a bot' not in clean) and ('dmca removal request for' not in clean):
+				cleaned_comments.append(clean)
 
-    return cleaned_comments
+	return cleaned_comments
 
 
 def tokenize_lda(cleaned_comments, bigram_model=None, trigram_model=None):
-    '''
-    Tokenize each cleaned comment, remove stopwords, and
-    create bigram/trigrams
+	'''
+	Tokenize each cleaned comment, remove stopwords, and
+	create bigram/trigrams
 
-    Parameters
-    ----------
-    cleaned_comments : list of strings of cleaned comments
-    bigram_model : pre-trianed gensim Phrases model for 2-grams
-    trigram_model : pre-trianed gensim Phrases model for 3-grams
+	Parameters
+	----------
+	cleaned_comments : list of strings of cleaned comments
+	bigram_model : pre-trianed gensim Phrases model for 2-grams
+	trigram_model : pre-trianed gensim Phrases model for 3-grams
 
-    Returns
-    -------
-    comments_words : list of tokenized comments
-    bigram_model : same as input, or fit to tokenized comments if input is None
-    trigram_model : same as input, or fit to tokenized comments if input is None
-    '''
+	Returns
+	-------
+	comments_words : list of tokenized comments
+	bigram_model : same as input, or fit to tokenized comments if input is None
+	trigram_model : same as input, or fit to tokenized comments if input is None
+	'''
 
-    stop_words = stopwords.words('english')
-    stop_words.extend(['im','ive','dont','get','youre','would','thats',
-                       'really','one','also','something','even','thing','things','must',
-                       'cant','much','could','way','lot','got','get','go','like','th'])
+	stop_words = stopwords.words('english')
+	stop_words.extend(['im','ive','dont','get','youre','would','thats',
+					   'really','one','also','something','even','thing','things','must',
+					   'cant','much','could','way','lot','got','get','go','like','th'])
 
-    # Convert each comment to a list of words, removing any missed punctuation
-    word_posts = []
-    for post in comments_words:
-        word_posts.append(gensim.utils.simple_preprocess(post, deacc=True))
-    comments_words = word_posts
+	# Convert each comment to a list of words, removing any missed punctuation
+	word_posts = []
+	for post in cleaned_comments:
+		word_posts.append(gensim.utils.simple_preprocess(post, deacc=True))
+	comments_words = word_posts
 
-    # Remove words which are very common
-    cleaned_posts = []
-    for post in comments_words:
-        cleaned_post = []
-        for word in post:
-            if word not in stop_words:
-                cleaned_post.append(word)
-        cleaned_posts.append(cleaned_post)
-    comments_words = cleaned_posts
+	# Remove words which are very common
+	cleaned_posts = []
+	for post in comments_words:
+		cleaned_post = []
+		for word in post:
+			if word not in stop_words:
+				cleaned_post.append(word)
+		cleaned_posts.append(cleaned_post)
+	comments_words = cleaned_posts
 
-    # Create bigrams - two words which are commonly used together
-    if bigram_model == None:
-        bigram_model = gensim.models.Phrases(comments_words, min_count=15, threshold=200)
-    bigrammized_posts = []
-    for post in comments_words:
-        bigrammized_posts.append(bigram_model[post])
-    comments_words = bigrammized_posts
+	# Create bigrams - two words which are commonly used together
+	if bigram_model == None:
+		bigram_model = gensim.models.Phrases(comments_words, min_count=15, threshold=200)
+	bigrammized_posts = []
+	for post in comments_words:
+		bigrammized_posts.append(bigram_model[post])
+	comments_words = bigrammized_posts
 
-    # Create trigrams - three words which are commonly used together
-    if trigram_model == None:
-        trigram_model = gensim.models.Phrases(bigram_model[comments_words], threshold=200)
-    trigrammized_posts = []
-    for post in comments_words:
-        trigrammized_posts.append(trigram_model[bigram_model[post]])
-    comments_words = trigrammized_posts
+	# Create trigrams - three words which are commonly used together
+	if trigram_model == None:
+		trigram_model = gensim.models.Phrases(bigram_model[comments_words], threshold=200)
+	trigrammized_posts = []
+	for post in comments_words:
+		trigrammized_posts.append(trigram_model[bigram_model[post]])
+	comments_words = trigrammized_posts
 
-    return comments_words, bigram_model, trigram_model
+	return comments_words, bigram_model, trigram_model
 
 
 def create_dictionary(comments_words):
-    '''
-    Generate gensim dictionary for tokenized comment corpus
+	'''
+	Generate gensim dictionary for tokenized comment corpus
 
-    Parameters
-    ----------
-    comments_words : list of tokenized comments
+	Parameters
+	----------
+	comments_words : list of tokenized comments
 
-    Returns
-    -------
-    comments_bow : list of bow representations of comments in corpus
-    dictionary : gensim dictionary mapping indices to token
-    '''
+	Returns
+	-------
+	comments_bow : list of bow representations of comments in corpus
+	dictionary : gensim dictionary mapping indices to token
+	'''
 
-    dictionary = corpora.Dictionary(comments_words)
-    comments_bow = [dictionary.doc2bow(comment) for comment in comments_words]
-    
-    return comments_bow, dictionary
+	dictionary = corpora.Dictionary(comments_words)
+	comments_bow = [dictionary.doc2bow(comment) for comment in comments_words]
+	
+	return comments_bow, dictionary
