17c27f9e2f44916fad5a8f9a300ff858f4f8202d
bzhulex <bzhu75@gatech.edu>
2022-04-15 03:45:20 -0400

    add contextualized topic modeling driver

--- ctm_driver.py
+++ ctm_driver.py
@@ -22,7 +22,6 @@ import gensim
 from gensim.models import CoherenceModel
 from gensim.corpora.dictionary import Dictionary
 
-
 parser = argparse.ArgumentParser()
 parser.add_argument('--n_topics', type=int, default=15)
 parser.add_argument('--weekly_series', action="store_true")
@@ -30,7 +29,6 @@ parser.add_argument('--daily_series', action="store_true")
 parser.add_argument('--load', action="store_true")
 parset.add_argument('--ctm_load', action="store_true")
 parser.set_defaults(feature=False)
-
 
 
 if __name__ == '__main__':
@@ -78,3 +76,26 @@ if __name__ == '__main__':
 		stop_words.extend(['im','ive','dont','get','youre','would','thats',
 				'really','one','also','something','even','thing','things','must',
 				'cant','much','could','way','lot','got','get','go','like','th'])
+
+		topic_words = ctm.get_topics(20)
+		topics = [(i, ctm.get_word_distribution_by_topic_id(i)[:20]) for i in range(len(topic_words))]
+		topic_dict = dict()
+		fig, axs = plt.subplots(3, 3, figsize=(20,8))
+		cloud = WordCloud(stopwords=stop_words,background_color='white')
+		k=0
+		for i in range(3):
+		  for j in range(3):
+		    while k not in topics_sorted_by_coherence:
+		      k+=1
+		    for topic in topics:
+		      if topic[0] == k:
+		        topic_dict = dict(topic[1])
+		    cloud.generate_from_frequencies(topic_dict, max_font_size=300)
+		    axs[i,j].imshow(cloud)
+		    axs[i,j].set_title('Topic ' + str(k) + r' | $C_v$: ' + '{:.2f}'.format(per_topic_coherence[k]), fontsize=26)
+		    axs[i,j].axis('off')
+		    k+=1
+		plt.tight_layout()
+		plt.savefig("CTM_Word_Cloud.png")
+		plt.close()
+		plt.clf()
